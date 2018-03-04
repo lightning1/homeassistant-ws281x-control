0639d6026827efed1e92b733eb73e733a293716b
Felix Kluge <f.kluge@agdsn.de>
2018-03-04 13:35:20 +0100

    Change to single PWM strip

--- framework/strip.py
+++ framework/strip.py
@@ -4,9 +4,10 @@ import colorsys
 
 
 # LED strip configuration:
-LED_COUNT      = 284     # Number of LED pixels.
-#LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+#LED_COUNT      = 284     # Number of LED pixels. (for Atrium-Corners)
+LED_COUNT      = 150     # Number of LED pixels. (for balls)
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
